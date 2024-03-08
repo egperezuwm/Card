d2653c4370e6b4fe3381a320278ecd9d53e7c931
egperezuwm <Enrique.Perez.MKE@gmail.com>
2024-03-08 15:44:57 -0600

    Organizing files that make up a playing card.

---
+++ Tests/testPlayingCard.py
@@ -0,0 +1,19 @@
+from Card import Card
+import unittest
+
+class TestInit(unittest.TestCase):
+  def test_default(self):
+      card = Card()
+      #there is NOT a default constructor as a card MUST be instantiated with Suit and Rank
+
+  def test_oneArg(self):
+      # there is NOT a default constructor as a card MUST be instantiated with Suit and Rank
+      card = Card(10)
+      card = Card(Card.Rank.ACE)
+      card = Card(Card.Suit.SPADE)
+
+  def test_oneArg(self):
+      # the card should be constructed of from the Card class's ENUM class Rank and Suit.
+      card = Card("ACE", 10)
+      card = Card("SPADE", Card.Rank.ACE)
+      card = Card(Card.Suit.SPADE)
